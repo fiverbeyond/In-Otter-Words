a75f2f7fd0e0c1e65a608b8d81dcf5f11a4ad5ad
FiverBeyond <evanawitt@gmail.com>
2025-05-08 14:54:12 -0400

    Cleaned up a bit.

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1746577145',resources: [
+    cacheName: 'chirpy-1746639110',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
